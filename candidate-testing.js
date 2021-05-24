19df7ec3ef78d6a8b1e64a11dfae58b7f798c961
amcquirk <>
2021-05-24 18:07:01 +0000

    removed line 6, 7, 8, 9, and 10 parantheses per Cassie recommendation

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName=(" ");
+let candidateName=" ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question=("1) Who was the first American woman in space? ");
-let correctAnswer=("Sally Ride");
-let candidateAnswer=(" ");
+let question="1) Who was the first American woman in space? ";
+let correctAnswer="Sally Ride";
+let candidateAnswer=" ";
 let questions;
 let correctAnswers;
 let candidateAnswers;
