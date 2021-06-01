953fe2654eb25f724972532f1c7451b2040f655a
amcquirk <>
2021-06-01 18:28:55 +0000

    commented out old code from askQuestions function and added in new code

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,13 +5,13 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName=" ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question="1) Who was the first American woman in space? ";
-let correctAnswer="Sally Ride";
-let candidateAnswer=" ";
-let questions;
-let correctAnswers;
-let candidateAnswers;
 
+let questions=["1)Who was the first American woman in space?", "2)True or false: 5 kilomater == 5000 meters?", "3)(5+3)/2*10","4)Given the array [8,'Orbit','Trajectory',45], what entry is at index 2?", "5)What is the minimum crew size for the ISS?"
+]
+
+let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
+
+let candidateAnswers=[];
 
 function askForName() {let candidateName=input.question("Candidate Name: ");
 
@@ -20,7 +20,7 @@ console.log("Hello, "+candidateName+"!");
 
 };
 
-function askQuestion() {let candidateAnswer=input.question("1) Who was the first American woman in space? ");
+function askQuestion() /*{let candidateAnswer=input.question("1) Who was the first American woman in space? ");
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
 if (candidateAnswer==correctAnswer) {
@@ -28,11 +28,60 @@ if (candidateAnswer==correctAnswer) {
 } else {console.log("your answer is incorrect");
 
 }}
+*/
 
-function gradeQuiz(candidateAnswers) {
+{for (let i=0; i<questions.length; i++){
+  let answer=input.question(questions[i]);
+  candidateAnswers.push(answer);
+
+  console.log(`Your answer: ${candidateAnswers[i]}; Correct answer: ${correctAnswers[i]}`);
+}
+
+
+/*
+
+for (let i=0; i<questions.length-4; i++) {
+
+  candidateAnswers[0]=input.question(questions[0])
+
+
+ console.log(`Your answer: ${candidateAnswers[0]};
+Correct answer: ${correctAnswers[0]}`);
+
+
+console.log(" ");
+
+ candidateAnswers[1]=input.question(questions[1])
+
+ console.log(`Your answer: ${candidateAnswers[1]};
+Correct answer: ${correctAnswers[1]}`);
+
+console.log(" ");
+
+ candidateAnswers[2]=input.question(questions[2])
+
+ console.log(`Your answer: ${candidateAnswers[2]};
+Correct answer: ${correctAnswers[2]}`);
+
+console.log(" ");
+
+ candidateAnswers[3]=input.question(questions[3])
+
+ console.log(`Your answer: ${candidateAnswers[3]};
+Correct answer: ${correctAnswers[3]}`);
+
+console.log(" ");
+
+ candidateAnswers[4]=input.question(questions[4])
+
+ console.log(`Your answer: ${candidateAnswers[4]}
+Correct answer: ${correctAnswers[4]}`);
+
+}
+*/
+function gradeQuiz(candidateAnswers)
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
 
   let grade;
   
@@ -44,7 +93,7 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   
-  askQuestion();
+  askQuestion() 
   gradeQuiz(this.candidateAnswers);
 }
 
